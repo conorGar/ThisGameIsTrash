8b61b8d04fd1d409fd8f37a23719fb9210e279fa
stevegreat <stevegreat@gmail.com>
2018-07-13 19:06:55 -0700

    Changes for a Room and RoomPortal mock up.  Scene is "RoomTest". Jim gets a box collider. tk2d Camera is centered for easier calculations. Both I've set to 0,0 local space. Added a little extension to get random elements from a List.

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// Information about a room.  A world is made up of a series of rooms.
+public class Room : MonoBehaviour
+{
+    public RoomManager roomManager;
+
+    public int roomNum;
+    public List<EnemySpawn> enemySpawns;
+    List<Enemy> enemies;
+    public List<Friend> friends;
+    
+
+    [System.Serializable]
+    public class EnemySpawn
+    {
+        public List<Enemy> enemies;
+    }
+
+    private void ActivateRoom()
+    {
+        roomManager.mainCamera.ScreenCamera.ViewportToWorldPoint(transform.position);
+    }
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        var enemies = new List<Enemy>();
+        bool allowArmoredEnemies = false;
+        if (GlobalVariableManager.Instance.DAY_NUMBER < 2)
+            allowArmoredEnemies = true;
+
+        // enemy spawns
+        for (int i=0; i < enemySpawns.Count; ++i)
+        {
+            // get a random enemy from the enemy spawn list
+            Enemy enemy = enemySpawns[i].enemies.RandomElement();
+
+            // ignore armored enemies if they aren't allowed to spawn yet.
+            if (enemy.IsArmored && !allowArmoredEnemies)
+                continue;
+
+            enemies.Add(enemy);  
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/RoomPortal.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomPortal : MonoBehaviour {
+    public RoomManager roomManager;
+
+    public enum Direction
+    {
+        HORIZONTAL,
+        VERTICAL
+    }
+
+    public Direction direction = Direction.HORIZONTAL;
+    public Room positiveRoom; // The room that is +x or +y depending on the direction
+    public Room negativeRoom; // The room that is -x or -y depending on the direction
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            if (roomManager.currentRoom == positiveRoom)
+            {
+                Debug.Log("Entering negative room!");
+                roomManager.currentRoom = negativeRoom;
+                roomManager.previousRoom = positiveRoom;
+                roomManager.isTransitioning = true;
+                roomManager.lerpCamera = 0.0f;
+            }
+            else if (roomManager.currentRoom == negativeRoom)
+            {
+                Debug.Log("Entering positive room!");
+                roomManager.currentRoom = positiveRoom;
+                roomManager.previousRoom = negativeRoom;
+                roomManager.isTransitioning = true;
+                roomManager.lerpCamera = 0.0f;
+            }
+            else
+            {
+                Debug.Log("This portal isn't even for the current room.  How did you get here!?");
+            }
+        }
+    }
+}
