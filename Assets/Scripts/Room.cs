6862b1ca01d6f0607d5d5f554390a63ba6fe0430
JamesGar <garij34@mail.sunysuffolk.edu>
2018-07-17 20:30:57 -0400

    Added portals and boundaries to all world 1 rooms

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -36,17 +36,17 @@ public class Room : MonoBehaviour
     {
     	Debug.Log("ActivateRoom....activated");
         roomManager.mainCamera.ScreenCamera.ViewportToWorldPoint(transform.position);
-<<<<<<< HEAD
 //<<<<<<< HEAD
-        roomManager.SetCamFollowBounds(leftPortal.transform.position.x,rightPortal.transform.position.x,
-        								topPortal.transform.position.y,botPortal.transform.position.y);
+//<<<<<<< HEAD
+       // roomManager.SetCamFollowBounds(leftPortal.transform.position.x,rightPortal.transform.position.x,
+       // 								topPortal.transform.position.y,botPortal.transform.position.y);
 //=======
         roomManager.SetCamFollowBounds(this);
 //>>>>>>> refs/remotes/origin/digital_smash
-=======
+//=======
         //roomManager.SetCamFollowBounds(leftPortal.transform.position.x,rightPortal.transform.position.x,
         							//	topPortal.transform.position.y,botPortal.transform.position.y);
->>>>>>> refs/heads/MeleeSystemImprovements
+//>>>>>>> refs/heads/MeleeSystemImprovements
     }
 
 
