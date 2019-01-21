c087c741260d5ed90f7a4bbc6ccef0078d1f1401
conorGar <conorGar@gmail.com>
2019-01-21 13:37:55 -0500

    Trash Door Blockades function

--- Assets/Behaviors/GlobalVariableManager.cs
+++ Assets/Behaviors/GlobalVariableManager.cs
@@ -90,6 +90,13 @@ public class GlobalVariableManager : UserDataItem {
 
 	public TUTORIALPOPUPS TUT_POPUPS_SHOWN = TUTORIALPOPUPS.NONE;
 
+	public enum TRASHDOORS{
+		NONE =	0,
+		W1_11TRASH =	1<<0,
+	}
+
+	public TRASHDOORS BROKEN_TRASH_DOORS = TRASHDOORS.NONE;
+
     //-------------Enemy Global Variables------------//
     public enum BOSSES : int
     {
@@ -263,6 +270,7 @@ public class GlobalVariableManager : UserDataItem {
         json_data["UPGRADES"] = (uint)UPGRADES_UNLOCKED;
 
         json_data["TUT_POPUPS_SHOWN"] = (uint)TUT_POPUPS_SHOWN;
+		json_data["BROKEN_TRASH_DOORS"] = (uint)BROKEN_TRASH_DOORS; //steve did I do this right?
 
         return json_data;
     }
@@ -314,6 +322,8 @@ public class GlobalVariableManager : UserDataItem {
         UPGRADES_UNLOCKED = (UPGRADES)json_data["UPGRADES"].AsInt;
 
         TUT_POPUPS_SHOWN = (TUTORIALPOPUPS)json_data["TUT_POPUPS_SHOWN"].AsInt;
+		BROKEN_TRASH_DOORS = (TRASHDOORS)json_data["BROKEN_TRASH_DOORS"].AsInt; //Steve did I do this right?
+
     }
 
     // helpers
