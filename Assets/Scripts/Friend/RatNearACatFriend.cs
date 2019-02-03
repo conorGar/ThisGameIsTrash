1cb9822ad70222af83efeafbf6491a2919b420e8
conorGar <conorGar@gmail.com>
2019-02-02 21:42:18 -0500

    Rat Near A Cat Friend Structure

--- Assets/Scripts/Friend/RatNearACatFriend.cs
+++ Assets/Scripts/Friend/RatNearACatFriend.cs
@@ -4,16 +4,51 @@ using System.Collections;
 public class RatNearACatFriend : Friend
 {
 
-	// Use this for initialization
+
+	public GameObject timeBack;
+	public Hub_TimeUpgradeStand timeStand;
+
+	public override void GenerateEventData()
+    {
+		switch (GetFriendState()) {
+			case "INTRO":
+				day = -1;
+				break;
+			case "HUB_INTRO":
+				day = CalendarManager.Instance.currentDay;
+				timeStand.enabled = false;
+				break;
+			case "END":
+				day = CalendarManager.Instance.currentDay;
+				timeStand.enabled = true;
+				break;
+		}
+        
+    }
+
 	void Start ()
 	{
 	
 	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	
+
+	public IEnumerator TimeBackShow(){
+		timeBack.SetActive(true);
+		yield return new WaitForSeconds(3f);
+		dialogManager.ReturnFromAction();
 	}
+	
+	public override void OnUpdate()
+    {
+        switch (GetFriendState()) {
+            case "INTRO":
+                nextDialog = "RatCatIntro";
+                GetComponent<ActivateDialogWhenClose>().Execute();
+                break;
+            case "HUB_INTRO":
+                nextDialog = "RatCat1";
+                GetComponent<ActivateDialogWhenClose>().Execute();
+                break;
+        }
+    }
 }
 
