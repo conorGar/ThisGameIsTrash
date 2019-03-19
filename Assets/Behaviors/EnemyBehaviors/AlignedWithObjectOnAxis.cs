61038a915a5c8c1b30bfee21aa411f1d20ccda10
stevegreat <stevegreat@gmail.com>
2019-03-19 13:59:22 -0700

    Adding a leapifier helper class that handles gameObjects that "leap" and their shadow translations.  Hooked it up to Ev_Enemy_Slasher

--- Assets/Behaviors/EnemyBehaviors/AlignedWithObjectOnAxis.cs
+++ Assets/Behaviors/EnemyBehaviors/AlignedWithObjectOnAxis.cs
@@ -12,17 +12,17 @@ public enum ALIGNED_AXIS
 public class AlignedWithObjectOnAxis : MonoBehaviour {
     public GameObject alignedObject;
     public ALIGNED_AXIS alignedAxis;
-    public float threshhold;
+    public float threshold;
 
     public bool IsAligned()
     {
         switch (alignedAxis) {
             case ALIGNED_AXIS.X_AXIS:
-                if (Mathf.Abs(gameObject.transform.position.x - alignedObject.transform.position.x) < threshhold)
+                if (Mathf.Abs(gameObject.transform.position.x - alignedObject.transform.position.x) < threshold)
                     return true;
                 break;
             case ALIGNED_AXIS.Y_AXIS:
-                if (Mathf.Abs(gameObject.transform.position.y - alignedObject.transform.position.y) < threshhold)
+                if (Mathf.Abs(gameObject.transform.position.y - alignedObject.transform.position.y) < threshold)
                     return true;
                 break;
         }
